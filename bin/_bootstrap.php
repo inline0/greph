f7b2dd01be044905229714985e3e2b99927e64c1
dnnsjsk <dnnsjsk@gmail.com>
2026-04-09 09:32:57 +0200

    Enable benchmarking older base checkouts

--- bin/_bootstrap.php
+++ bin/_bootstrap.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
-$phgrepRoot = dirname(__DIR__);
+$phgrepRoot = getenv('PHGREP_ROOT');
+
+if (!is_string($phgrepRoot) || $phgrepRoot === '') {
+    $phgrepRoot = dirname(__DIR__);
+}
+
+$phgrepRoot = rtrim($phgrepRoot, DIRECTORY_SEPARATOR);
 
 require $phgrepRoot . '/vendor/autoload.php';
